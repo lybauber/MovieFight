62b8d7f2716489cdd101c0834c5c5abca424c1ff
Brandon <lybauber@gmail.com>
2023-08-22 20:50:37 -0500

    creando busqueda y html

--- index.js
+++ index.js
@@ -5,18 +5,32 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
         }
     });
-    console.log(response.data)
+
+    if (response.data.Error){
+        return [];
+    }
+    return response.data.Search;
 }
 
 
 const input = document.querySelector('input');
-let timeoutId;
-const onInput = event =>{
-    if(timeoutId){
-        clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(()=> {
-        fetchData(event.target.value);
-    }, 1000);
+
+
+
+
+const onInput = async event =>{
+   const movies = await fetchData(event.target.value);
+   
+   for (let movie of movies){
+    const div = document.createElement('div');
+
+    div.innerHTML = ` 
+        <img src="${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+    `;
+    document.querySelector('#target').appendChild(div);
+   }
+
+   
 }
-input.addEventListener('input', onInput);
+input.addEventListener('input', debounce(onInput));
